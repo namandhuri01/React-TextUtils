40f7c35d75713c60530cda4ff9ac9d7341721847
Naman mehta <namandhuri01@gmail.com>
2021-08-28 22:18:50 +0530

    remoce react router dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@
 import "./App.css";
 import Home from "./components/Home";
 import Alert from "./components/Alert";
-import About from "./components/About";
 import Navbar from "./components/Navbar";
 import React, { useState } from 'react'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
 function App() {
 	const [mode, setMode] = useState('light');
 	const [alert, setAlert] = useState(null);
@@ -37,20 +36,20 @@ function App() {
 	}
   return (
     <>
-     <Router>
+     {/* <Router> */}
         <Navbar title="React Text Format" mode={mode} toggleMode={toggleMode} key={new Date()} />
         <div className="container mb-3" >
 			<Alert alert={alert}/>
-			<Switch>
+			{/* <Switch>
 				<Route exact path="/about">
 					<About />
 				</Route>
-				<Route exact path="/">
+				<Route exact path="/"> */}
 					<Home showAlert={showAlert} heading="Enter Your Text Here" mode={mode} />
-				</Route>
-			</Switch>
+				{/* </Route>
+			</Switch> */}
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
