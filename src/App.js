2e87e18497492e9436482662d6f56ffd8c535a62
Naman mehta <namandhuri01@gmail.com>
2021-08-28 21:51:40 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Home from "./components/Home";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import React, { useState } from 'react'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
+	const [mode, setMode] = useState('light');
+	const [alert, setAlert] = useState(null);
+	const showAlert = (message, type)=>{
+		setAlert({
+			message:message,
+			type:type
+		});
+		setTimeout(() => {
+			setAlert(null);
+		}, 1500);
+	}
+	const toggleMode = ()=>{
+		if(mode === 'light'){
+			setMode('dark');
+			document.body.style.backgroundColor = '#042743';
+			showAlert("Dark mode has been enabled", "success");
+		}
+		if(mode === 'dark'){
+			setMode('light');
+			document.body.style.backgroundColor = '#fff';
+			showAlert("Light mode has been enabled", "success");
+		}
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Router>
+        <Navbar title="React Text Format" mode={mode} toggleMode={toggleMode} key={new Date()} />
+        <div className="container mb-3" >
+			<Alert alert={alert}/>
+			<Switch>
+				<Route exact path="/about">
+					<About />
+				</Route>
+				<Route exact path="/">
+					<Home showAlert={showAlert} heading="Enter Your Text Here" mode={mode} />
+				</Route>
+			</Switch>
+          	
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+
+function Home(props) {
+    const [text, setText] = useState('');
+    const btndisabel = text.length===0 ? true : false;
+    const handleUppercase= ()=>{
+       let newtext = text.toUpperCase();
+       setText(newtext);
+       props.showAlert('Converted to uppercase','success');
+    }
+    const handleLowercase= ()=>{
+        let newtext = text.toLowerCase();
+        setText(newtext);
+        props.showAlert('Converted to lowercase!','success');
+    }
+    const handleCapitilize= ()=>{
+        var newtext = text.split(' ');  
+        var CapitalizedWords = [];  
+        newtext.forEach(element => {  
+            CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));  
+        });  
+        setText( CapitalizedWords.join(' '));
+        props.showAlert('Text Capatilized!!','success');
+    }
+    const handleCopyToText= ()=>{
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+    const handleClearText= ()=>{
+        let newtext = '';
+        setText(newtext);
+        props.showAlert('Text Cleared!','success');
+    }
+    const handleRemoveSpace= ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+    
+  return (
+    <>
+        <div className="form-group mt-3">
+            <label htmlFor="text-field" className="mb-3 h3" style={{color: props.mode==='dark'?'white':'#042743'}}>{props.heading}</label>
+            <textarea className="form-control" id="text-field" rows="5" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}}></textarea>
+        </div>
+        <div className="mt-3">
+            <button type="button" disabled={btndisabel} className="btn btn-primary mx-2" onClick={handleUppercase}>Uppercase All</button>
+            <button type="button" disabled={btndisabel} className="btn btn-primary mx-2" onClick={handleLowercase}>Lowecase All</button>
+            <button type="button" disabled={btndisabel} className="btn btn-primary mx-2" onClick={handleCapitilize}>Capitilize</button>
+            <button type="button" disabled={btndisabel} className="btn btn-primary mx-2" onClick={handleCopyToText}>Copy Text</button>
+            <button type="button" disabled={btndisabel} className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
+            <button type="button" disabled={btndisabel} className="btn btn-primary mx-2" onClick={handleRemoveSpace}>Remove Extra Spaces</button>
+        </div>
+        <div className="mt-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h3 >Your text summary</h3>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Character Count {text.length} Word Count</p>
+            <p>{0.008 * text.split(' ').filter((element)=>{return element.length!==0}).length} Minutes Read</p>
+            <p>Preview</p>
+            <p>{text}</p>
+        </div>
+    </>
+  );
+}
+
+export default Home;
